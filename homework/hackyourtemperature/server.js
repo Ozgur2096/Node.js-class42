11a82514f9bad14e12589e81ff1317952c304d86
Ozgur Ozhan <ozgur2096@gmail.com>
2023-03-10 18:01:29 +0100

    node.js week2 homework completed

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,19 +1,6 @@
-import express from 'express';
-
-const app = express();
+import app from './app.js';
 
 const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.send('hello from backend to frontend!');
-});
-
-app.use(express.json());
-app.post('/weather', (req, res) => {
-  const cityName = req.body.cityName;
-  res.send(cityName);
-});
-
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
---
+++ homework/hackyourtemperature/sources/keys.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'e4443a3428b1abef91f5d969b24d2689';
