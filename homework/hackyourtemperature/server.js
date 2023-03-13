f49e40d19042de94fe52a4bc469e277fef23f6f0
Ozgur Ozhan <ozgur2096@gmail.com>
2023-03-13 13:04:40 +0100

    homework revised

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,16 +1,21 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
 
-app.use(express.json());
 app.post('/weather', (req, res) => {
   const cityName = req.body.cityName;
+  if (typeof cityName === 'undefined') {
+    res.status(400);
+    res.send('Enter a city name, please');
+  }
+  res.status(200);
   res.send(cityName);
 });
 
